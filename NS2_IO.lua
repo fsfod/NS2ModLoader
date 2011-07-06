12dc554cba627188460be141219a7c0992d2dc83
fsfod <fsfod@esdf.zzz>
2011-07-06 10:31:59 +0100

    Redesigned NS2_IO lua bootstrapping code
    Added support to run from a directory that's not in the Natural Selection 2 directory either through one of the startup bats or manually creating ModPath.lua in the mods folder with the contents being ModPath = PATHOFMODDIRETORY or maybe created by an installer
    Added some error checking to startup bat files also added support for 64 bit this is dry code through

--- NS2_IO.lua
+++ NS2_IO.lua
@@ -1,16 +1,102 @@
-local print = Shared.Message
 
-if(not NS2_IO) then
-	local ModuleEntryPoint, msg, where = package.loadlib("ModLoader/NS2_IO.dll", "luaopen_NS2_IO")
+NS2IOLoader = {
+  MarkerFile = "NS2_IO.dll",
+  DefaultDirectory = "ModLoader",
+  ModulePath = "NS2_IO.dll",
+  EntryPoint = "luaopen_NS2_IO"
+}
+
+local ForwardSlash, BackSlash = string.byte("/"), string.byte("\\")
+
+local function JoinPaths(path1, path2)
+
+	local firstChar = string.byte(path2) 
 	
-	if(ModuleEntryPoint) then
-		ModuleEntryPoint()
-	else
-		--yes checking for a localized error string :( but its best we can do
-		if(string.find(msg,"The specified module could not be found.")) then
-			print("Unable to load NS2_IO.dll Please make sure you extacted this mod to a folder named ModLoader in your natrual selection 2 directory")
+	if(path1 == "") then
+	  return path2
+	end
+	
+	if(firstChar == ForwardSlash or firstChar == BackSlash) then
+		local lastChar = string.byte(path1, #path1)
+		
+		if(lastChar == ForwardSlash or lastChar == BackSlash) then
+			return path1..string.sub(path2, 2)
 		else
-			error("failed to load NS2_IO.dll module because of error:\n "..msg)
+			return path1..path2
+		end
+	else
+		local lastChar = string.byte(path1, #path1)
+		
+		if(lastChar == ForwardSlash or lastChar == BackSlash) then
+			return path1..path2
+		else
+			return path1.."/"..path2
 		end
 	end
+end
+
+function NS2IOLoader:TryLoad(BasePath)
+  assert(type(BasePath) == "string" and BasePath ~= "")
+  
+  local ModuleEntryPoint, msg, where = package.loadlib(JoinPaths(BasePath, self.ModulePath), self.EntryPoint)
+  
+  if(ModuleEntryPoint) then
+		 local result
+		 result, msg = pcall(ModuleEntryPoint)
+		 
+		 if(result) then
+		   self.Loaded = true
+		  return true
+		 end
+  end
+  
+  return false, msg
+end
+
+function NS2IOLoader:Load()
+
+  if(self.Loaded) then
+    return true
+  end
+
+  local BasePath, result, errorMsg
+
+  if(not self:FileExists(self.MarkerFile)) then
+    return false, string.format("Marker file %s does not seem to exist in the mods directory does not seem to of been fully extracted/copyed correctly", self.MarkerFile)
+  end
+
+  if(self:FileExists("ModPath.lua")) then
+    Script.Load("ModPath.lua")
+    
+    if(ModPath and type(ModPath) == "string" and ModPath ~= "") then
+      BasePath = ModPath
+    end
+  end
+
+  if(not BasePath) then
+    BasePath = self.DefaultDirectory
+    
+    if(not self:FileExists(JoinPaths(JoinPaths("..", self.DefaultDirectory), self.MarkerFile))) then
+      return false, string.format("Mod was not launched the with helper bat or extracted to a directory named %s in the Natural Selection 2 directory.", self.DefaultDirectory)
+    end
+  end
+
+  return self:TryLoad(BasePath)
+end
+
+--this fails if -game is a zip, max seems to of not implemented file matching for zips
+function NS2IOLoader:FileExists(file)
+  local matchingFiles = {}
+  
+  Shared.GetMatchingFileNames(file, false, matchingFiles)
+  
+  local lfile = file:lower()
+  
+  for _,path in ipairs(matchingFiles) do
+    if(path:lower() == lfile) then
+      return true
+    end
+  end
+  
+  return false
 end
